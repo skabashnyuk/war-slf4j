cd294612bb52129ae71fc57d9ec1c7349c9678f7
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-11-18 17:39:33 +0200

    Fix config

---
+++ src/main/java/org/eclipse/che/Slf4jTest.java
@@ -0,0 +1,41 @@
+package org.eclipse.che;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Extend HttpServlet class
+public class Slf4jTest extends HttpServlet {
+
+  private static final Logger LOG = LoggerFactory.getLogger(Slf4jTest.class);
+
+  private String message;
+
+  public void init() throws ServletException {
+    // Do required initialization
+    message = "Hello World";
+  }
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    for (int i = 0; i < 100; i++) {
+      LOG.info("This is a message");
+    }
+    // Set response content type
+    response.setContentType("text/html");
+
+    // Actual logic goes here.
+    PrintWriter out = response.getWriter();
+    out.println("<h1>" + message + "</h1>");
+  }
+
+  public void destroy() {
+    // do nothing.
+  }
+}
